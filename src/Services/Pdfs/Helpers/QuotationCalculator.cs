c76df1a38db7fbcda7e05154d2c1b9738c80b63b
De Murel Giovany <demurelgiovany@hotmail.com>
2023-11-14 13:39:35 +0100

    refactoring QuotationDocument

---
+++ src/Services/Pdfs/Helpers/QuotationCalculator.cs
@@ -0,0 +1,72 @@
+﻿using Domain.Common;
+using Domain.Formulas;
+using Foodtruck.Shared.Quotations;
+using Foodtruck.Shared.Supplements;
+using Services.Pdfs.QuotationPdfs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Pdfs.Helpers;
+
+static class QuotationCalculator
+{
+    public static int CalculateSupplementVatPercentage(QuotationSupplementLineDto supplementLine)
+    {
+        return (int) (supplementLine.SupplementVat / supplementLine.SupplementPrice * 100M);
+    }
+
+    public static Money CalculateSupplementLineTotalPrice(QuotationSupplementLineDto supplementLine)
+    {
+        return new Money((supplementLine.SupplementPrice * supplementLine.Quantity));
+    }
+
+    public static Money CalculateSupplementLineTotalVat(Money totalprice, int vatPercentage)
+    {
+        return new Money(totalprice.Value * vatPercentage / 100M);
+    }
+
+    public static Money CalculateFormulaPrice(QuotationModel model)
+    {
+        decimal formulaPrice = model.FoodtruckPrice;
+        if (model.FormulaSupplementLines != null)
+        {
+            foreach (var item in model.FormulaSupplementLines)
+            {
+                formulaPrice += (item.SupplementPrice * item.Quantity);
+            }
+            
+        }
+        return new Money(formulaPrice);
+    }
+
+    public static Money CalculateFormulaVat(decimal totalprice)
+    {
+        var vatPercentage = 21;
+        return new Money(totalprice * vatPercentage / 100M);
+    }
+
+    internal static Money CalculateExtraSupplementsTotalPrice(IEnumerable<QuotationSupplementLineDto>? extraSupplementLines)
+    {
+        decimal totalPrice = 0;
+        foreach (var item in extraSupplementLines)
+        {
+            totalPrice += item.SupplementPrice * item.Quantity;
+        }
+        return new Money(totalPrice);
+    }
+
+    internal static Money CalculateExtraSupplementsTotalVat(IEnumerable<QuotationSupplementLineDto>? extraSupplementLines)
+    {
+        decimal totalVat = 0;
+        foreach (var item in extraSupplementLines)
+        {
+            int itemVat = CalculateSupplementVatPercentage(item);
+            Money itemTotalPrice = CalculateSupplementLineTotalPrice(item);
+            totalVat += CalculateSupplementLineTotalVat(itemTotalPrice, itemVat).Value;
+        }
+        return new Money(totalVat);
+    }
+}
--- src/Services/Pdfs/QuotationPdfs/QuotationDocument.cs
+++ src/Services/Pdfs/QuotationPdfs/QuotationDocument.cs
@@ -1,9 +1,12 @@
 ﻿using Domain.Common;
 using Foodtruck.Shared.Customers;
+using Foodtruck.Shared.Quotations;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using QuestPDF.Elements;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
+using Services.Pdfs.Helpers;
 
 namespace Services.Pdfs.QuotationPdfs;
 public class QuotationDocument : IDocument
@@ -14,9 +17,6 @@ public class QuotationDocument : IDocument
     {
         Model = model;
     }
-
-    DateTime tomorrow = new DateTime().AddDays(1);
-    DateTime nextMonth = new DateTime().AddDays(31);
 
     Random randomNumber = new Random();
 
@@ -42,145 +42,13 @@ public class QuotationDocument : IDocument
             });
     }
 
-    void ComposeContent(IContainer container)
-    {
-        container.PaddingVertical(40).Column(column =>
-        {
-            column.Spacing(20);
-
-            column.Item().Row(row =>
-            {
-                row.RelativeItem().Component(new CustomerComponent(Model.Customer));
-                row.ConstantItem(50);
-                row.RelativeItem().Column(column =>
-                {
-                    column.Item().Component(new AddressComponent("Factuuradres", Model.BillingAddress));
-                    column.Item().Component(new AddressComponent("Evenementadres", Model.EventAddress));
-                });
-            });
-
-            column.Item().BorderBottom(1).BorderColor(Colors.Grey.Darken1).Element(ComposeFormulaSupplementsTable);
-
-            //column.Item().BorderBottom(1).BorderColor(Colors.Grey.Darken1).Element(ComposeExtraSupplementsTable);
-
-
-            if (!string.IsNullOrWhiteSpace(Model.ExtraInfo))
-                column.Item().PaddingTop(25).Element(ComposeComments);
-        });
-    }
-
-    void ComposeFormulaSupplementsTable(IContainer container)
-    {
-        container.Table(table =>
-        {
-            table.ColumnsDefinition(columns =>
-            {
-                columns.RelativeColumn(2);
-                columns.RelativeColumn(6);
-                columns.RelativeColumn(3);
-                columns.RelativeColumn(3);
-                columns.RelativeColumn(3);
-            });
-            table.Header(header =>
-            {
-                header.Cell().Element(CellStyle).AlignCenter().Text("Aantal");
-                header.Cell().Element(CellStyle).AlignCenter().Text("Omschrijving");
-                header.Cell().Element(CellStyle).AlignCenter().Text("Eenhprijs (€)");
-                header.Cell().Element(CellStyle).AlignCenter().Text("Bedrag (€)");
-                header.Cell().Element(CellStyle).AlignCenter().Text("BTW");
-
-                static IContainer CellStyle(IContainer container)
-                {
-                    return container.DefaultTextStyle(x => x.SemiBold().LineHeight((float)1.8)).PaddingVertical(0).Border(1).BorderColor(Colors.Grey.Darken1).Background(Colors.Grey.Lighten1);
-                }
-            });
-            table.Cell().Element(CellStyle).AlignCenter().Text($"1");
-            table.Cell().Element(CellStyle).Text($"Mobiele bar Blanche btw 21%");
-            table.Cell().Element(CellStyle).AlignRight().Text($"{new Money(Model.FoodtruckPrice)}");
-            table.Cell().Element(CellStyle).AlignRight().Text($"{new Money(Model.FoodtruckPrice)}");
-            table.Cell().Element(CellStyle).AlignRight().Text($"€{new Money((Model.FoodtruckPrice) * 21 / 100M)}");
-
-            foreach (var item in Model.FormulaSupplementLines)
-            {
-                table.Cell().Element(CellStyle).AlignCenter().Text($"");
-                table.Cell().Element(CellStyle).Text($"incl. {item.Quantity}x {item.Name} btw {(int)(item.SupplementVat/item.SupplementPrice*100)}%");
-                table.Cell().Element(CellStyle).AlignRight().Text($"");
-                table.Cell().Element(CellStyle).AlignRight().Text($"");
-                table.Cell().Element(CellStyle).AlignRight().Text($"");
-            }
-
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-
-            decimal pricePerGuest = 12;
-            table.Cell().Element(CellStyle).AlignCenter().Text($"{Model.NumberOfGuests}");
-            table.Cell().Element(CellStyle).Text($"{Model.Formula.Title} {Model.NumberOfGuests} personen BTW 12%");
-            table.Cell().Element(CellStyle).AlignRight().Text($"{new Money(pricePerGuest)}");
-            table.Cell().Element(CellStyle).AlignRight().Text($"{new Money(Model.NumberOfGuests * pricePerGuest)}");
-            table.Cell().Element(CellStyle).AlignRight().Text($"€{new Money(Model.NumberOfGuests * pricePerGuest * 12M / 100M)}");
-
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).Text("");
-
-            foreach (var item in Model.ExtraSupplementLines)
-            {
-                table.Cell().Element(CellStyle).AlignCenter().Text($"{item.Quantity}");
-                table.Cell().Element(CellStyle).Text($"{item.Name} btw {(int)(item.SupplementVat / item.SupplementPrice * 100)}%");
-                table.Cell().Element(CellStyle).AlignRight().Text($"{new Money(item.SupplementPrice)}");
-                table.Cell().Element(CellStyle).AlignRight().Text($"{new Money(item.SupplementPrice * item.Quantity)}");
-                table.Cell().Element(CellStyle).AlignRight().Text($"€{new Money((item.SupplementPrice * item.Quantity) * item.SupplementVat / 100M)}");
-            }
-
-            table.Cell().Element(FooterHeader).AlignCenter().Text("Belastbaar");
-            table.Cell().ColumnSpan(2).Element(FooterHeader).AlignCenter().Text("BTW");
-            table.Cell().ColumnSpan(2).Element(FooterHeader).AlignCenter().Text("Totaal");
-
-            decimal totaltaxable = 0;
-            decimal totalbtw = 0;
-            foreach (var item in Model.FormulaSupplementLines)
-            {
-                totaltaxable += item.SupplementPrice * item.Quantity;
-                totalbtw += item.SupplementPrice * item.SupplementVat / 100M * item.Quantity;
-            }
-            foreach (var item in Model.ExtraSupplementLines)
-            {
-                totaltaxable += item.SupplementPrice * item.Quantity;
-                totalbtw += item.SupplementPrice * item.SupplementVat / 100M * item.Quantity;
-            }
-            totaltaxable += Model.FoodtruckPrice;
-            totaltaxable += Model.NumberOfGuests * pricePerGuest;
-
-            totalbtw += Model.FoodtruckPrice * 21M / 100M;
-            totalbtw += Model.NumberOfGuests * pricePerGuest * 12M /100M;
-
-            table.Cell().Element(CellStyle).AlignCenter().PaddingTop(1).PaddingBottom(1).Text($"{new Money(Model.Price)}");
-            table.Cell().ColumnSpan(2).Element(CellStyle).AlignCenter().PaddingTop(1).PaddingBottom(1).Text($"{new Money(Model.VatTotal)}");
-            table.Cell().ColumnSpan(2).Element(CellStyle).AlignCenter().PaddingTop(1).PaddingBottom(1).Text($"{new Money(Model.Price + Model.VatTotal)}");
-
-            static IContainer FooterHeader(IContainer container)
-            {
-                return container.DefaultTextStyle(x => x.FontSize(11).LineHeight((float)1.8)).PaddingVertical(0).Border(1).BorderBottom(0).BorderColor(Colors.Grey.Darken1).Background(Colors.Grey.Lighten2);
-            }
-
-            static IContainer CellStyle(IContainer container)
-            {
-                return container.BorderVertical(1).BorderColor(Colors.Grey.Medium).PaddingHorizontal(5);
-            }
-        });
-    }
     void ComposeHeader(IContainer container)
     {
         container.Row(row =>
-        {   
+        {
             row.RelativeItem().Column(column =>
             {
-                column.Item().PaddingVertical(20).Text("blanche-logo.png");
+                column.Item().Width(200).Height(50).Placeholder();
                 column
                     .Item().Text($"Offerte #10{randomNumber.NextInt64(25, 99)}")
                     .FontSize(14).SemiBold().FontColor(Colors.Grey.Darken3);
@@ -209,6 +77,152 @@ public class QuotationDocument : IDocument
         });
     }
 
+    void ComposeContent(IContainer container)
+    {
+        container.PaddingVertical(40).Column(column =>
+        {
+            column.Spacing(20);
+
+            column.Item().Row(row =>
+            {
+                row.RelativeItem().Component(new CustomerComponent(Model.Customer));
+                row.ConstantItem(50);
+                row.RelativeItem().Column(column =>
+                {
+                    column.Item().Component(new AddressComponent("Factuuradres", Model.BillingAddress));
+                    column.Item().Component(new AddressComponent("Evenementadres", Model.EventAddress));
+                });
+            });
+
+            column.Item().BorderBottom(1).BorderColor(Colors.Grey.Darken1).Element(ComposeQuotationLinesTable);
+
+            //column.Item().BorderBottom(1).BorderColor(Colors.Grey.Darken1).Element(ComposeExtraSupplementsTable);
+
+
+            if (!string.IsNullOrWhiteSpace(Model.ExtraInfo))
+                column.Item().PaddingTop(25).Element(ComposeComments);
+        });
+    }
+
+
+
+    void ComposeQuotationLinesTable(IContainer container)
+    {
+        container.Table(table =>
+        {
+            Money formulaTotalPrice = QuotationCalculator.CalculateFormulaPrice(Model);
+            Money formulaTotalVat = QuotationCalculator.CalculateFormulaVat(formulaTotalPrice.Value);
+
+            Money extraSupplementsTotalPrice = QuotationCalculator.CalculateExtraSupplementsTotalPrice(Model.ExtraSupplementLines);
+            Money extraSupplementsTotalVat = QuotationCalculator.CalculateExtraSupplementsTotalVat(Model.ExtraSupplementLines);
+
+            Money QuotationTotalPrice = new Money(formulaTotalPrice.Value + extraSupplementsTotalPrice.Value);
+            Money QuotationTotalVat = new Money(formulaTotalVat.Value + extraSupplementsTotalVat.Value);
+
+
+            table.ColumnsDefinition(columns =>
+            {
+                columns.RelativeColumn(2);
+                columns.RelativeColumn(6);
+                columns.RelativeColumn(3);
+                columns.RelativeColumn(3);
+                columns.RelativeColumn(3);
+            });
+
+            table.Header(header =>
+            {
+                header.Cell().Element(CellStyle).AlignCenter().Text("Aantal");
+                header.Cell().Element(CellStyle).AlignCenter().Text("Omschrijving");
+                header.Cell().Element(CellStyle).AlignCenter().Text("Eenhprijs (€)");
+                header.Cell().Element(CellStyle).AlignCenter().Text("Bedrag (€)");
+                header.Cell().Element(CellStyle).AlignCenter().Text("BTW");
+
+                static IContainer CellStyle(IContainer container)
+                {
+                    return container.DefaultTextStyle(x => x.SemiBold().LineHeight((float)1.8)).PaddingVertical(0).Border(1).BorderColor(Colors.Grey.Darken1).Background(Colors.Grey.Lighten1);
+                }
+            });
+            table.Cell().Element(CellStyle).AlignCenter().Text("1");
+            table.Cell().Element(CellStyle).Text($"Formule: {Model.Formula.Title}").SemiBold();
+            table.Cell().Element(CellStyle).AlignRight().Text($"{formulaTotalPrice}");
+            table.Cell().Element(CellStyle).AlignRight().Text($"{formulaTotalPrice}");
+            table.Cell().Element(CellStyle).AlignRight().Text($"€{formulaTotalVat}");
+
+            table.Cell().Element(CellStyle).AlignCenter().Text("");
+            table.Cell().Element(CellStyle).Text($"Mobiele bar Blanche btw 21%");
+            table.Cell().Element(CellStyle).AlignRight().Text($"");
+            table.Cell().Element(CellStyle).AlignRight().Text($"");
+            table.Cell().Element(CellStyle).AlignRight().Text($"");
+
+
+            ComposeSupplementLines(table, Model.FormulaSupplementLines);
+            TableRowSpacer(table);
+            ComposeSupplementLines(table, Model.ExtraSupplementLines);
+            ComposeQuotationTotals(table, QuotationTotalPrice, QuotationTotalVat);
+
+            static IContainer CellStyle(IContainer container)
+            {
+                return container.BorderVertical(1).BorderColor(Colors.Grey.Medium).PaddingHorizontal(5);
+            }
+        });
+    }
+
+    private void ComposeSupplementLines(TableDescriptor table,IEnumerable<QuotationSupplementLineDto> supplementLines)
+    {
+        foreach (var item in supplementLines)
+        {
+            int supplementVatPercentage = QuotationCalculator.CalculateSupplementVatPercentage(item);
+            Money supplementLineTotalPrice = QuotationCalculator.CalculateSupplementLineTotalPrice(item);
+            Money supplementLineTotalVat = QuotationCalculator.CalculateSupplementLineTotalVat(supplementLineTotalPrice, supplementVatPercentage);
+
+            table.Cell().Element(CellStyle).AlignCenter().Text($"{item.Quantity}");
+            table.Cell().Element(CellStyle).AlignLeft().Text($"{item.Name} btw {supplementVatPercentage}%");
+            table.Cell().Element(CellStyle).AlignRight().Text($"{new Money(item.SupplementPrice)}");
+            table.Cell().Element(CellStyle).AlignRight().Text($"{supplementLineTotalPrice}");
+            table.Cell().Element(CellStyle).AlignRight().Text($"€{supplementLineTotalVat}");
+        }
+
+        static IContainer CellStyle(IContainer container)
+        {
+            return container.BorderVertical(1).BorderColor(Colors.Grey.Medium).PaddingHorizontal(5);
+        }
+    }
+
+    private void ComposeQuotationTotals(TableDescriptor table, Money QuotationTotalPrice, Money QuotationTotalVat)
+    {
+        table.Cell().Element(FooterHeader).AlignCenter().Text("Belastbaar");
+        table.Cell().ColumnSpan(2).Element(FooterHeader).AlignCenter().Text("BTW");
+        table.Cell().ColumnSpan(2).Element(FooterHeader).AlignCenter().Text("Totaal");
+
+        table.Cell().Element(CellStyle).AlignCenter().PaddingTop(1).PaddingBottom(1).Text($"{QuotationTotalPrice}");
+        table.Cell().ColumnSpan(2).Element(CellStyle).AlignCenter().PaddingTop(1).PaddingBottom(1).Text($"{QuotationTotalVat}");
+        table.Cell().ColumnSpan(2).Element(CellStyle).AlignCenter().PaddingTop(1).PaddingBottom(1).Text($"{new Money(QuotationTotalPrice.Value + QuotationTotalVat.Value)}");
+
+        static IContainer FooterHeader(IContainer container)
+        {
+            return container.DefaultTextStyle(x => x.FontSize(11).LineHeight((float)1.8)).PaddingVertical(0).Border(1).BorderBottom(0).BorderColor(Colors.Grey.Darken1).Background(Colors.Grey.Lighten2);
+        }
+
+        static IContainer CellStyle(IContainer container)
+        {
+            return container.BorderVertical(1).BorderColor(Colors.Grey.Medium).PaddingHorizontal(5);
+        }
+    }
+
+    private void TableRowSpacer(TableDescriptor table)
+    {
+        table.Cell().Element(CellStyle).AlignCenter().Text("");
+        table.Cell().Element(CellStyle).AlignLeft().Text("");
+        table.Cell().Element(CellStyle).AlignRight().Text("");
+        table.Cell().Element(CellStyle).AlignRight().Text("");
+        table.Cell().Element(CellStyle).AlignRight().Text("");
+        static IContainer CellStyle(IContainer container)
+        {
+            return container.BorderVertical(1).BorderColor(Colors.Grey.Medium).PaddingHorizontal(5);
+        }
+    }
+
+
     void ComposeComments(IContainer container)
     {
         container.ShowEntire().Background(Colors.Grey.Lighten3).Padding(10).Column(column =>
@@ -222,12 +236,12 @@ public class QuotationDocument : IDocument
 
 public class CustomerComponent : IComponent
 {
-    public string? Firstname { get; set; }
-    public string? Lastname { get; set; }
-    public string? Email { get; set; }
-    public string? Phone { get; set; }
-    public string? CompanyName { get; set; }
-    public string? CompanyNumber { get; set; }
+    public string? Firstname { get; }
+    public string? Lastname { get;}
+    public string? Email { get; }
+    public string? Phone { get; }
+    public string? CompanyName { get; }
+    public string? CompanyNumber { get; }
     public CustomerComponent(CustomerDto.Detail customer ) 
     { 
         Firstname = customer.Firstname; 
@@ -278,3 +292,4 @@ public class AddressComponent : IComponent
         });
     }
 }
+
